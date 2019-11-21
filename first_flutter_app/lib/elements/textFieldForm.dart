0204eef438df45964988725682905c3a08590bad
PRE <>
2019-11-21 17:17:03 +0800

    no message

---
+++ first_flutter_app/lib/elements/textFieldForm.dart
@@ -0,0 +1,25 @@
+// 表单
+
+import 'package:flutter/material.dart';
+
+class TextFieldForm extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("表单"),
+      ),
+      body: Center(
+        child: new TextFieldFormRoute(),
+      ),
+    );
+  }
+}
+class  TextFieldFormRoute  extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Center(
+
+    );
+  }
+}
