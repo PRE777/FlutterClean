0371ff40d0ed99f0ea4099576605c607e2abe391
PRE <>
2019-11-26 16:57:53 +0800

    1.装饰容器DecoratedBox

--- first_flutter_app/lib/containerWidget/elements/constrainedBox.dart
+++ first_flutter_app/lib/containerWidget/elements/constrainedBox.dart
@@ -71,12 +71,41 @@ class ConstrainedBoxRoute extends StatelessWidget {
               ),
             ),
           ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: SizedBox(
+            width: 200,
+            height: 50,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.red, Colors.orange[700]]), //背景渐变
+                borderRadius: BorderRadius.circular(3.0), //3像素圆角
+                boxShadow: [
+                  //阴影
+                  BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(2.0, 2.0),
+                      blurRadius: 4.0),
+                ],
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 16.0),
+                child: Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
         )
       ],
     );
   }
 }
 
+// 装饰容器 DecoratedBox
 Widget redBox = DecoratedBox(
   decoration: BoxDecoration(color: Colors.red),
 );
@@ -85,4 +114,22 @@ Widget greenBox = DecoratedBox(
 );
 Widget blueBox = DecoratedBox(
   decoration: BoxDecoration(color: Colors.blue),
+);
+
+Widget decorationBox = DecoratedBox(
+  decoration: BoxDecoration(
+      gradient: LinearGradient(colors: [Colors.red, Colors.orange[700]]), //背景渐变
+      borderRadius: BorderRadius.circular(3.0), //3像素圆角
+      boxShadow: [
+        //阴影
+        BoxShadow(
+            color: Colors.black54, offset: Offset(2.0, 2.0), blurRadius: 4.0)
+      ]),
+  child: Padding(
+    padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 18.0),
+    child: Text(
+      "Login",
+      style: TextStyle(color: Colors.white),
+    ),
+  ),
 );
